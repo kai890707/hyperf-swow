296f0f5efce1b9b2144168107f67d43668df42db
蔡銘凱 <z85385637@gmail.com>
2023-09-23 04:06:21 +0800

    Dev : append coroutine in controller

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -16,6 +16,7 @@ use App\Anser\Orchestrators\V2\CreateOrderOrchestrator;
 // use Psr\Log\LoggerInterface;
 // use Hyperf\Logger\LoggerFactory;
 use App\Utils\Log;
+use Hyperf\Utils\Coroutine;
 
 class IndexController extends AbstractController
 {
@@ -36,13 +37,17 @@ class IndexController extends AbstractController
         $data      = $this->request->all();
         $memberKey = $data["memberKey"];
         $products  = $data["products"];
+        Coroutine::create(function () use ($memberKey,$products) {
+            $createOrder = new CreateOrderOrchestrator();
 
-        $createOrder = new CreateOrderOrchestrator();
+            $data = $createOrder->build($products, $memberKey);
+            $result = $data ?? ["order_key" => $createOrder->orderKey];
+            Log::getInstance()->info(json_encode($result));
+            // $this->logger->info(json_encode($result));
+            return $this->response->json($result);
+        });
+        
 
-		$data = $createOrder->build($products, $memberKey);
-	    $result = $data ?? ["order_key" => $createOrder->orderKey];
-        Log::getInstance()->info(json_encode($result));
-        // $this->logger->info(json_encode($result));
-        return $this->response->json($result);
+        
     }
 }
